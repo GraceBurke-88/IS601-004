6894a456a232faf95debcb1f60b7a40b4b31e71e
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2023-02-24 10:51:58 -0500

    basic add functionality

---
+++ M4/MyCalc.py
@@ -0,0 +1,11 @@
+#running total/value called ans
+ans = 0
+
+#function for addition
+def add(num1,num2):
+    ans = num1 + num2
+    print(num1, "+", num2, '=', ans)
+    return ans
+
+#test
+print(add(1,2))
