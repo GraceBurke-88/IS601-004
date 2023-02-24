502616c2317736a4fdcd5384e6f20f7d5cb5a7f6
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2023-02-24 12:49:15 -0500

    Defining main logic (user input for addition)

--- M4/MyCalc.py
+++ M4/MyCalc.py
@@ -41,3 +41,36 @@ def div(num1,num2):
 #test
 print(div(1,2))
 
+#Define a "main" logic to run when the program runs
+#Ask for user input
+
+#operator = input("Enter math:")
+#ans = 0
+
+print(type(ans))
+
+while True:
+    calc = input("Enter the your calculation or exit to quit: ")
+
+    #User can input exit to stop using the calculator 
+    #use calc.lower in case they input 'Exit or EXIT'
+    if calc.lower() == 'exit':
+        print("Bye now, exiting program.")
+        break
+
+    #If input is 'ans + 2' it does not realize 'ans' is a variable just 
+    #considers it as a string -->
+    #ValueError: could not convert string to float: 'ans'
+    if 'ans' in calc:
+        calc = calc.replace('ans', str(ans))
+
+    if '+' in calc:
+        print('Calculating addition')
+        num1, num2 = calc.split('+')
+        ans = add(float(num1),float(num2))
+        print(ans)
+
+    else:
+        print('The calculation is not a valid operation.')
+
+
