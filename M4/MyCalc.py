d303d7cfc1094aaa0e579c3c90ab27e86e2c61d8
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2023-02-24 11:01:05 -0500

    basic sub, mult, div functionality

--- M4/MyCalc.py
+++ M4/MyCalc.py
@@ -1,6 +1,7 @@
 #running total/value called ans
 ans = 0
 
+''' gnb5 implemented on 2/24/23 '''
 #function for addition
 def add(num1,num2):
     ans = num1 + num2
@@ -9,3 +10,34 @@ def add(num1,num2):
 
 #test
 print(add(1,2))
+
+''' gnb5 implemented on 2/24/23 '''
+#function for subtraction
+def sub(num1,num2):
+    ans = num1 - num2
+    print(num1, "-", num2, '=', ans)
+    return ans
+
+#test
+print(sub(1,2))
+
+''' gnb5 implemented on 2/24/23 '''
+#function for multiplication
+def mult(num1,num2):
+    ans = num1 * num2
+    print(num1, "x", num2, '=', ans)
+    return ans
+
+#test
+print(mult(1,2))
+
+''' gnb5 implemented on 2/24/23 '''
+#function for division
+def div(num1,num2):
+    ans = num1 / num2
+    print(num1, "/", num2, '=', ans)
+    return ans
+
+#test
+print(div(1,2))
+
